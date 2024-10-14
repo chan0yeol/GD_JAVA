a1f14829be8e9fdb11a73e35eed62afbdd5ed698
GD <GD@GD12>
2024-10-14 14:42:59 +0900

    문자형 실습 'z' 의 정수값을 구하고 16진수로 변환 후 unicode로 출력해라

---
+++ java02_Pratice/src/com/goo/dee/CharHome.java
@@ -0,0 +1,13 @@
+package com.goo.dee;
+
+public class CharHome {
+	public void zConvert() {
+		char zChar = 'z';
+		System.out.println("'z'의 정수의 값 :" + (int)zChar);
+		System.out.println("'z'의 16진수의 값 : " + Integer.toHexString(zChar));
+		char unicodeChar = '\u007a';
+		System.out.println("'z'를 unicode표기법 으로 출력 : " + unicodeChar);
+	}
+	//연산 : 'z'는 정수의 값이 무엇인가?
+	// 정수의 값을 hexa 값으로 만든 후 , Unicode 표기법 으로 선언 및 대입하고 출력 
+}
---
+++ java02_Pratice/src/com/goo/dee/PracMain.java
@@ -0,0 +1,10 @@
+package com.goo.dee;
+
+public class PracMain {
+
+	public static void main(String[] args) {
+		CharHome c = new CharHome();
+		c.zConvert();
+	}
+
+}
