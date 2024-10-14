3247f6c5a919e3409ad8a8c8c0ccaa3ac29c0831
GD <GD@GD12>
2024-10-14 12:19:07 +0900

    import와 정수자료형의 연산 실습

--- java02_primitive_type/src/com/example/edu/DataType.java
+++ java02_primitive_type/src/com/example/edu/DataType.java
@@ -7,9 +7,30 @@ public class DataType {
 		// Byte = -128 ~ 127
 		byte b = 127; // 1byte는 01111111 가 최대 정수 입력 크기
 //		byte not_in = 128; // byte의 최대 크기가 127이기 때문에 128의 값(리터럴)은 대입하지 못함.     
-		
-	}
-	public static void main(String[] args) {
+		byte b1 = 127;
+		byte b2 = 1;
+		byte sum = (byte) (b1 + b2); // 자바에서의 정수 연산의 결과는 무조건 int 타입이다.
+		System.out.println(sum); // -128이 출력된다.
+
+		// int의 최대값에 1을 더한다
+		int i2 = 100;
+		int i3 = 200;
+		int sumInt = i2 + i3;
+		System.out.println(sumInt);
+		int maxInt = Integer.MAX_VALUE;
+		System.out.println("int의 최대값 : " + maxInt);
+		int calInt = 1;
+		int maxPlus = maxInt + calInt;
+		System.out.println("max 연산의 결과 : " + maxPlus);
+
+		long l = 1;
+		long longCal = maxInt + l; // 정수연산에서 long이 참여하면 결과는 long크기로 반환한다.
+		System.out.println("long 연산 결과 : " + longCal);
+
+		short maxShort = Short.MAX_VALUE;
+		short s1 = 1;
+		short calShort = (short) (maxShort + s1);
+		System.out.println(calShort);
 
 	}
 
---
+++ java02_primitive_type/src/com/goo/dee/DataTypeMain.java
@@ -0,0 +1,15 @@
+package com.goo.dee;
+
+//TODO 003 import를 확인하기 위한 main 메소드를 포함하고 있는 클래스
+import com.example.edu.DataType;
+
+public class DataTypeMain {
+
+	// TODO 004 com.example.edu.DataType의 클래스 들을 생성하여 호출 하거나 호출하기 위한 실행메소드
+	// jvm 이 처음 자동으로 호출하는 메소드
+	public static void main(String[] args) {
+		DataType dt = new DataType(); // 클래스를 사용하기 위해 메모리에 올리는 방법 new = instance화 객체생성
+		dt.declaring_assignment(); // 호출
+	}
+
+}
